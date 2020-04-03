ca8e71a36596d778795f9b154254ff4f7304a457
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-02 23:59:15 -0300

    add real time to frontend, adding create and list tweets

--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Tweet.css';
 import Like from '../like.svg';
 import api from '../services/api';
 
-export default class Tweet extends Component {
+export default function Tweet({ tweet }) {
 
-  handleLike = async () => {
-    const { _id } = this.props.tweet;
-    await api.post(`/likes/${_id}`);
-  };
-
-  render() {
-    const { tweet } = this.props;
-    
-    return (
-      <li className="tweet">
-        <strong>{ tweet.author }</strong>
-        <p>{ tweet.content }</p>
-        <button type="button" onClick={this.handleLike}>
-          <img src={Like} alt="like" />
-          { tweet.likes }
-        </button>
-      </li>  
-    );
+  async function handlelike(){
+    const {_id} = tweet;
+    await api.post(`/likes/${_id}`)
   }
+  return (
+    <li className="tweet">
+      <strong>{tweet.author}</strong>
+      <p>{tweet.content}</p>
+      <button type="button" onClick={handlelike}>
+        <img src={Like} alt="like" />
+        {tweet.likes }
+      </button>
+    </li>
+  )
 }
--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
-// import {useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import socket from 'socket.io-client';
+
 import api from '../services/api';
 import './Timeline.css';
+import Tweet from '../components/Tweet';
+
+
 import twitterLogo from '../twitter.svg';
 
 export default function Timeline() {
 
   const [newTweet, setNewTweet] = useState('');
+  const [tweets, setTweets] = useState([])
+
+  useEffect(() => {
+    async function getTweets(){
+      const response = await api.get('/tweets');
+      setTweets(response.data);
+    }
+    getTweets();
+    subscribeToEvents();
+    
+  }, [])
 
   async function handleNewTweet(e) {
-    if (e.keyCode !== 13) return;    
-
-    
+    if (e.keyCode !== 13) return;
     const content = newTweet;
     const author = localStorage.getItem('@GoTwitter:username')
-    
+
     await api.post('/tweets', {
       author, content
     })
-    
+
     setNewTweet('');
+  }
+
+  function subscribeToEvents(){
+    const io = socket('http://localhost:3333');
+    io.on('tweet', data =>{
+      setTweets([data, ...tweets])
+    })
+    io.on('like', data =>{
+      setTweets(tweets.map(tweet=> tweet._id === data._id ? data : tweet))
+    })
   }
 
   return (
@@ -34,6 +57,13 @@ export default function Timeline() {
         />
       </form>
 
+
+      <ul className="tweet-list">
+        {tweets.map(tweet => (
+          <Tweet key={tweet._id} tweet={tweet} />
+        ))
+        }
+      </ul>
     </div>
   )
 }
