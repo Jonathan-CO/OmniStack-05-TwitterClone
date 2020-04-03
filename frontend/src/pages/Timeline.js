54d41d35db2e65c2f639a81cd6fe86ee01a817f5
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-02 23:17:57 -0300

    storaging tweet in databse with axios

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,4 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import {useHistory} from 'react-router-dom';
+import api from '../services/api';
+import './Timeline.css';
+import twitterLogo from '../twitter.svg';
+
 export default function Timeline() {
-  return <h1>Timeline</h1>
+
+  const [newTweet, setNewTweet] = useState('');
+
+  async function handleNewTweet(e) {
+    if (e.keyCode !== 13) return;    
+
+    
+    const content = newTweet;
+    const author = localStorage.getItem('@GoTwitter:username')
+    
+    await api.post('/tweets', {
+      author, content
+    })
+    
+    setNewTweet('');
+  }
+
+  return (
+    <div className="timeline-wrapper">
+      <img height={24} src={twitterLogo} alt="GoTwitter" />
+      <form>
+        <textarea
+          value={newTweet}
+          onChange={e => setNewTweet(e.target.value)}
+          onKeyDown={handleNewTweet}
+          placeholder="O que está acontecendo?"
+        />
+      </form>
+
+    </div>
+  )
 }
