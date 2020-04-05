54fd96f27ce83c21cba7892cd74762f8871685b9
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-05 16:32:42 -0300

    create the Timeline and adding the navigations

--- mobile/Twitter_Clone/src/pages/Timeline.js
+++ mobile/Twitter_Clone/src/pages/Timeline.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {View, StyleSheet} from 'react-native';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
+
+import api from '../services/api';
+import Tweet from '../components/Tweet';
+
 
 export default function Timeline (){
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(()=>{
+    async function getTweets(){
+      const response = await api.get('tweets');
+      setTweets(response.data)
+    }
+    getTweets();
+  },[])
+
   return (
-    <View />
+    <View style={styles.container}>
+      <FlatList 
+      data={tweets}
+      keyExtractor={tweet => tweet._id}
+      renderItem={({item}) =>  <Tweet tweet={item}/>}
+      />
+     
+    </View>
   )
 }
 const styles = StyleSheet.create({
