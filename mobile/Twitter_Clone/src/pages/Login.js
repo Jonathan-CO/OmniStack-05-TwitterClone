08548fc1d7da56ba7a143554715c844d296c6b85
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-04 22:28:49 -0300

    create app structure with RocketSeat styles

--- mobile/Twitter_Clone/src/pages/Login.js
+++ mobile/Twitter_Clone/src/pages/Login.js
@@ -1,22 +1,51 @@
 import React from 'react';
 
-import {View, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function Login(){
     return (
-        <View style={styles.container}/>
+        <View style={styles.container}>
+            <Text>Bem Vindo ao Twitter Clone</Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container :{
+    container: {
         flex: 1,
-        backgroundColor: '#482',
-    },
-    content:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems:'center',
+        backgroundColor: "#FFF"
+      },
+    
+      content: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
         padding: 30
-    }
+      },
+    
+      input: {
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        height: 44,
+        paddingHorizontal: 15,
+        alignSelf: "stretch",
+        marginTop: 30
+      },
+    
+      button: {
+        height: 44,
+        alignSelf: "stretch",
+        marginTop: 10,
+        backgroundColor: "#4BB0EE",
+        borderRadius: 5,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+    
+      buttonText: {
+        color: "#FFF",
+        fontSize: 16,
+        fontWeight: "bold"
+      }
 })
